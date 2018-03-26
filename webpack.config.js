5c110ec9a583160670f2f3b60736d782e10c98fb
Andrew Drake <adrake@adrake.org>
2018-03-26 00:30:17 -0700

    Babel-ified webpack build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  devtool: 'source-map',
   entry: './index',
   output: {
     filename: 'bundle.js',
     library: 'Gloom',
     libraryTarget: 'umd',
     path: path.resolve(__dirname, 'dist'),
+  },
+
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 };
