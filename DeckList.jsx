0593f601883f86e2671e7db99afab6bd13cec5bc
Andrew Drake <adrake@adrake.org>
2018-04-07 23:48:07 -0700

    Control deck checkbox state in DeckList.

--- DeckList.jsx
+++ DeckList.jsx
@@ -9,25 +9,18 @@ export default class DeckList extends React.Component {
 
     this.globalLevelSelector = React.createRef();
     this.levelSelectors = {};
-    this.checkboxes = {};
+    this.state = { checked: new Set() };
     for (const { name } of Object.values(DECKS)) {
-      this.checkboxes[name] = React.createRef();
       this.levelSelectors[name] = React.createRef();
     }
 
+    this.handleCheckedChange = this.handleCheckedChange.bind(this);
     this.handleGlobalApplyClick = this.handleGlobalApplyClick.bind(this);
-  }
-
-  get_selection() {
-    return Object
-      .entries(this.checkboxes)
-      .filter(([, ref]) => ref.current.checked)
-      .map(([name]) => name);
   }
 
   get_selected_decks() {
     const result = [];
-    for (const name of this.get_selection()) {
+    for (const name of this.state.checked) {
       const deck = DECKS[name];
       deck.level = this.levelSelectors[name].current.get_selection();
       result.push(deck);
@@ -36,16 +29,24 @@ export default class DeckList extends React.Component {
   }
 
   set_selection(selected_deck_names) {
-    for (const ref of Object.values(this.checkboxes)) {
-      ref.current.checked = false;
-    }
     for (const deck_names of selected_deck_names) {
-      const checkbox = this.checkboxes[deck_names.name];
-      if (checkbox) {
-        checkbox.current.checked = true;
-        this.levelSelectors[deck_names.name].current.set_value(deck_names.level);
-      }
+      this.levelSelectors[deck_names.name].current.set_value(deck_names.level);
     }
+    this.setState({ checked: new Set(selected_deck_names.map(d => d.name)) });
+  }
+
+  handleCheckedChange(event) {
+    const name = event.target.value;
+    const isChecked = event.target.checked;
+    this.setState(({ checked: oldChecked }) => {
+      const checked = new Set(oldChecked);
+      if (isChecked) {
+        checked.add(name);
+      } else {
+        checked.delete(name);
+      }
+      return { checked };
+    });
   }
 
   handleGlobalApplyClick() {
@@ -58,7 +59,13 @@ export default class DeckList extends React.Component {
     const deckElements = Object.values(DECKS).map(({ name }) => (
       <li key={name}>
         <label>
-          <input name="deck" type="checkbox" value={name} ref={this.checkboxes[name]} />
+          <input
+            name="deck"
+            type="checkbox"
+            value={name}
+            checked={this.state.checked.has(name)}
+            onChange={this.handleCheckedChange}
+          />
           {name}
         </label>
         <LevelSelector inline text=" with level " ref={this.levelSelectors[name]} />
